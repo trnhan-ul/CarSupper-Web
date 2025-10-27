455e97cf5afc5feca8494564831eefa385162cec
trongnhan <trn.nhan2004@gmail.com>
2025-10-27 22:05:16 +0700

    fix: folder controllers

--- BE/src/controllers/ManageAccounts.js
+++ BE/src/controllers/ManageAccounts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Table, Button, Modal, Form, Spinner, InputGroup, Badge, Card } from "react-bootstrap";
 import { toast } from "react-toastify";
-import { getAllUsers, updateStatusUser } from "../../api/userApi";
+import { getAllUsers, updateStatusUser } from "../../../FE/src/api/userApi";
 import { FaSearch, FaUserEdit, FaCheckCircle, FaBan, FaTimesCircle } from "react-icons/fa";
 
 const ManageAccounts = () => {
